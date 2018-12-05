5f25e8e22ef08f02924989a59616e06bc37614b2
esbirol <esbirol@bu.edu>
2018-12-05 10:57:54 -0500

    GUI with popup
    this includes a popup window in the upcoming deadlines section

--- app/src/main/java/com/example/ecesu/courseplanner/UpcomingDeadlines.java
+++ app/src/main/java/com/example/ecesu/courseplanner/UpcomingDeadlines.java
@@ -1,13 +1,27 @@
 package com.example.ecesu.courseplanner;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class UpcomingDeadlines extends AppCompatActivity {
 
+    Button AddDeadline;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upcoming_deadlines);
+
+        AddDeadline = findViewById(R.id.AddDeadline);
+
+        AddDeadline.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent addingdeadline = new Intent(getApplicationContext(), PopActivity.class);
+                startActivity(addingdeadline);
+            }
+        });
     }
 }
