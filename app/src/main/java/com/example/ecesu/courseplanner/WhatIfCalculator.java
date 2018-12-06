a26a47da912284927288bf9339854d427e671c58
Gabriel <45494272+gramos0211@users.noreply.github.com>
2018-12-06 15:15:23 -0500

    made it work sisters

--- app/src/main/java/com/example/ecesu/courseplanner/WhatIfCalculator.java
+++ app/src/main/java/com/example/ecesu/courseplanner/WhatIfCalculator.java
@@ -2,10 +2,13 @@ package com.example.ecesu.courseplanner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import static java.lang.Double.*;
 
 public class WhatIfCalculator extends AppCompatActivity {
 
@@ -14,16 +17,25 @@ public class WhatIfCalculator extends AppCompatActivity {
     private EditText grade3;
     private EditText grade4;
     private EditText grade5;
-    private EditText weight1;
     private EditText grade6;
     private EditText grade7;
     private EditText grade8;
     private EditText grade9;
     private EditText grade10; //all of the grade IDs
+    private EditText weight1;
+    private EditText weight2;
+    private EditText weight3;
+    private EditText weight4;
+    private EditText weight5;
+    private EditText weight6;
+    private EditText weight7;
+    private EditText weight8;
+    private EditText weight9;
+    private EditText weight10;
     private TextView averagegrades; //text view, the number that will be displayed at the end
     double result; //the result variable for average grades
-    double g1, g2, g3, g4, g5, g6, g7, g8, g9, g10; //grade variables
-    double w1, w2, w3, w4, w5, w6, w7, w8, w9, w10; //weigh variables
+    double grades[]=new double[10]; //array for grades
+    double weights[]=new double[10]; //array for weights
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +54,16 @@ public class WhatIfCalculator extends AppCompatActivity {
         grade10= findViewById(R.id.grade10);
 
         weight1=findViewById(R.id.weight1);
+        weight2=findViewById(R.id.weight2);
+        weight3=findViewById(R.id.weight3);
+        weight4=findViewById(R.id.weight4);
+        weight5=findViewById(R.id.weight5);
+        weight6=findViewById(R.id.weight6);
+        weight7=findViewById(R.id.weight7);
+        weight8=findViewById(R.id.weight8);
+        weight9=findViewById(R.id.weight9);
+        weight10=findViewById(R.id.weight10);
+
 
         //button CALCULATE that calculates the end result
         Button whatif = findViewById(R.id.WhatIf);
@@ -49,58 +71,127 @@ public class WhatIfCalculator extends AppCompatActivity {
         //the text view variable of avarage grades,
         averagegrades = findViewById(R.id.average);
 
-        //basically saying when the CALCUALTE button is pressed it is going to do this
+        //basically saying when the CALCULATE button is pressed it is going to do this
         whatif.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v)
             {
                 //this is just trial. backend stuff doesn't work perfectly
-                //declaring the grade variables
-                Double grades[]=new Double[10];
-                grades[0] = Double.parseDouble(grade1.getText().toString());
-                grades[1] = Double.valueOf(grade2.getText().toString());
-                grades[2] = Double.valueOf(grade3.getText().toString());
-                grades[3] = Double.valueOf(grade4.getText().toString());
-                grades[4] = Double.valueOf(grade5.getText().toString());
-                grades[5] = Double.valueOf(grade6.getText().toString());
-                grades[6] = Double.valueOf(grade7.getText().toString());
-                grades[7] = Double.valueOf(grade8.getText().toString());
-                grades[8] = Double.valueOf(grade9.getText().toString());
-                grades[9] = Double.valueOf(grade10.getText().toString());
+                //if-else loops get text from edit fields and set them to 0.0 if there is no value in the field
+
+                if (!TextUtils.isEmpty(grade1.getText()))
+                    grades[0] = parseDouble(grade1.getText().toString());
+                else
+                    grades[0]=0.0;
+
+                if (!TextUtils.isEmpty(grade2.getText()))
+                    grades[1] = parseDouble(grade2.getText().toString());
+                else
+                    grades[1]=0.0;
+
+                if (!TextUtils.isEmpty(grade3.getText()))
+                    grades[2] = parseDouble(grade3.getText().toString());
+                else
+                    grades[2]=0.0;
+
+                if (!TextUtils.isEmpty(grade4.getText()))
+                    grades[3] = parseDouble(grade4.getText().toString());
+                else
+                    grades[3]=0.0;
+
+                if (!TextUtils.isEmpty(grade5.getText()))
+                    grades[4] = parseDouble(grade5.getText().toString());
+                else
+                    grades[4]=0.0;
+
+                if (!TextUtils.isEmpty(grade6.getText()))
+                    grades[5] = parseDouble(grade6.getText().toString());
+                else
+                    grades[5]=0.0;
+
+                if (!TextUtils.isEmpty(grade7.getText()))
+                    grades[6] = parseDouble(grade7.getText().toString());
+                else
+                    grades[6]=0.0;
+
+                if (!TextUtils.isEmpty(grade8.getText()))
+                    grades[7] = parseDouble(grade8.getText().toString());
+                else
+                    grades[7]=0.0;
+
+                if (!TextUtils.isEmpty(grade9.getText()))
+                    grades[8] = parseDouble(grade9.getText().toString());
+                else
+                    grades[8]=0.0;
+
+                if (!TextUtils.isEmpty(grade10.getText()))
+                    grades[9] = parseDouble(grade10.getText().toString());
+                else
+                    grades[9]=0.0;
 
                 //declaring the weight variables
-                Double weights[]=new Double[10];
-                weights[0] = Double.parseDouble(weight1.getText().toString());
-                weights[1] = Double.parseDouble(weight1.getText().toString());
-                weights[2] = Double.parseDouble(weight1.getText().toString());
-                weights[3] = Double.parseDouble(weight1.getText().toString());
-                weights[4] = Double.parseDouble(weight1.getText().toString());
-                weights[5] = Double.parseDouble(weight1.getText().toString());
-                weights[6] = Double.parseDouble(weight1.getText().toString());
-                weights[7] = Double.parseDouble(weight1.getText().toString());
-                weights[8] = Double.parseDouble(weight1.getText().toString());
-                weights[9] = Double.parseDouble(weight1.getText().toString());
+                if (!TextUtils.isEmpty(weight1.getText()))
+                    weights[0] = parseDouble(weight1.getText().toString());
 
-                //this only works if there are all 20 variables entered by the user!
-                //i don't know how to do it otherwise
-                //im trying these for loops to populates the empty values with zeros but it ain't working
-                for (int i=0;i<10;i++){
-                    if(weights[i]==0){
-                        weights[i]=0.0;
-                    }
-                    if(grades[i]==0){
-                        grades[i]=0.0;
-                    }
-                }
-                //loops for midterm average
-                
+                else
+                    weights[0]=0.0;
+
+                if (!TextUtils.isEmpty(weight2.getText()))
+                    weights[1] = parseDouble(weight2.getText().toString());
+                else
+                    weights[1]=0.0;
+
+                if (!TextUtils.isEmpty(weight3.getText()))
+                    weights[2] = parseDouble(weight3.getText().toString());
+                else
+                    weights[2]=0.0;
+
+                if (!TextUtils.isEmpty(weight4.getText()))
+                    weights[3] = parseDouble(weight4.getText().toString());
+                else
+                    weights[3]=0.0;
+
+                if (!TextUtils.isEmpty(weight5.getText()))
+                    weights[4] = parseDouble(weight5.getText().toString());
+                else
+                    weights[4]=0.0;
+
+                if (!TextUtils.isEmpty(weight6.getText()))
+                    weights[5] = parseDouble(weight6.getText().toString());
+                else
+                    weights[5]=0.0;
+
+                if (!TextUtils.isEmpty(weight7.getText()))
+                    weights[6] = parseDouble(weight7.getText().toString());
+                else
+                    weights[6]=0.0;
+
+                if (!TextUtils.isEmpty(weight8.getText()))
+                    weights[7] = parseDouble(weight8.getText().toString());
+                else
+                    weights[7]=0.0;
+
+                if (!TextUtils.isEmpty(weight9.getText()))
+                    weights[8] = parseDouble(weight9.getText().toString());
+                else
+                    weights[8]=0.0;
+
+                if (!TextUtils.isEmpty(weight10.getText()))
+                    weights[9] = parseDouble(weight10.getText().toString());
+                else
+                    weights[9]=0.0;
+
+
+                //loops for average
+
                 for (int i=0; i<10;i++){
                     result+=((grades[i]*weights[i])/100);
                 }
                 //testing if the result is shown when calculate is press
                 //yes the button works!
-                averagegrades.setText(Double.toString(result));
-
+                Double resulttext = parseDouble(String.format("%.2f",result));
+                averagegrades.setText(Double.toString(resulttext));
+                result = 0.0;  
             }
         });
 
