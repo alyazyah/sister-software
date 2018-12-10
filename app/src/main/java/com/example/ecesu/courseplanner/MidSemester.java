17feb04207008952155c8fa4d6fab9fdbd992f1b
chasityc7 <44933717+chasityc7@users.noreply.github.com>
2018-12-10 04:22:37 -0500

    Add files via upload

--- app/src/main/java/com/example/ecesu/courseplanner/MidSemester.java
+++ app/src/main/java/com/example/ecesu/courseplanner/MidSemester.java
@@ -2,7 +2,10 @@ package com.example.ecesu.courseplanner;
 
 import java.util.ArrayList;
 import java.lang.String;
+
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
@@ -13,14 +16,25 @@ import android.widget.CheckBox;
 import android.widget.ArrayAdapter;
 import android.view.View.OnClickListener;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.widget.ListView;
 import android.widget.Toast;
 import android.widget.TextView;
 
 public class MidSemester extends AppCompatActivity {
-
+    private ArrayList<String> arrayList;
+    private ArrayAdapter<String> adapter;
+    private EditText txtInput;
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adap;
+    private RecyclerView.LayoutManager layoutManager;
+    private Context context = this;
+    MidSemester midSemester = (MidSemester)context;
     Button save;
     EditText txtname, txttime, txtday;
-    TextView tv;
+    TextView tv, displayText;
+    ListView listView;
+
     View v;
 
     @Override
@@ -28,24 +42,47 @@ public class MidSemester extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mid_semester);
 
-        txtname = (EditText)findViewById(R.id.Name);
-        txttime = (EditText)findViewById(R.id.Time);
-        txtday = (EditText)findViewById(R.id.Day);
-        save = (Button)findViewById(R.id.Save);
-        tv=(TextView)findViewById(R.id.display);
+        arrayList = new ArrayList<>();
+        adapter = new ArrayAdapter<String>(this, R.layout.list_item, R.id.txtitem, arrayList);
+
+        txtname = (EditText) findViewById(R.id.Name);
+        txttime = (EditText) findViewById(R.id.Time);
+        txtday = (EditText) findViewById(R.id.Day);
+        save = (Button) findViewById(R.id.Save);
+        tv = (TextView) findViewById(R.id.display);
         tv.setMovementMethod(new ScrollingMovementMethod());
+        displayText = (TextView) findViewById((R.id.displayText));}
 
 
-        save.setOnClickListener(new OnClickListener() {
+        public void saveInfo (View view){
+            //save info of classes
 
-        @Override
-                public void onClick(View v){
-            tv.setText("Your Class: \n"+txtname.getText().toString()+"\n"+txtday.getText().toString()+"\n"+txttime.getText().toString()+"\n");
+            SharedPreferences classInput = getSharedPreferences("userClass", Context.MODE_PRIVATE);
+
+            SharedPreferences.Editor editObj = classInput.edit();
+            editObj.putString("courseName", txtname.getText().toString());
+            editObj.putString("courseTime", txttime.getText().toString());
+            editObj.putString("courseDays", txtday.getText().toString());
+            editObj.apply();
+
+            Toast.makeText(this, "Class Saved!", Toast.LENGTH_LONG).show();
         }
 
-    });
+        public void displayData (View view){
+            //print out saved classes
+            SharedPreferences classInput = getSharedPreferences("userClass", Context.MODE_PRIVATE);
 
-}
+            String nameOfClass = classInput.getString("courseName", " ");
+            String timeOfClass = classInput.getString("courseTime", " ");
+            String daysOfClass = classInput.getString("courseDays", " ");
+
+            displayText.setText("Class: " + nameOfClass + "\n" + "Time: " + timeOfClass + "\n" + "Days: " + daysOfClass);
+        }
+
+        recyclerView = findViewById(R.id.midsemester);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        adapter = new
 
 }
 
